8ef906c229adf5d315c1c4edd444ea9c8102088b
Mariam Sharaf <maryamshaider25@gmail.com>
2025-09-02 12:59:04 +0300

    first commit

---
+++ main_app/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=50, unique=True)
+    
+    class Meta:
+        db_table = 'countries'
+    
+    def __str__(self):
+        return self.country_name
+    
+    
+    
+class Visit(models.Model):
+    city_name = models.CharField(max_length=70)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='visits')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='visits', null=True)    
+    
+    class Meta:
+        db_table = 'visits'
+    
+    def __str__(self):
+        return self.city_name
+    
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    visit = models.ForeignKey(Visit, on_delete= models.CASCADE, related_name='comments')
+    
+    class Meta:
+        db_table = 'comments'
+    
+    def __str__(self):
+        return self.content
+    
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    comment = models.ForeignKey(Comment, on_delete= models.CASCADE, related_name='likes')
+    
+    class Meta:
+        db_table = 'likes'
+    
+    def __str__(self):
+        return f'{self.user} liked {self.comment}'
+    

94d8744e010acde770b78a4349a59ab24ac80a0a
Mariam Sharaf <maryamshaider25@gmail.com>
2025-09-02 20:20:37 +0300

    add visit list view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
    path('auth/signup/', views.SignUpView.as_view(), name='sign-up'),
    
+   path('visits/all/', views.VisitListView.as_view(), name='visit-list')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import UpdateView
+from .models import Visit, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from main_app.forms import SignupForm
 
 # Create your views here.
@@ -12,3 +12,8 @@ class SignUpView(CreateView):
     form_class = SignupForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
+    
+class VisitListView(ListView):
+    model = Visit
+    template_name = 'visit/visit-list.html'
+    context_object_name = 'visits'

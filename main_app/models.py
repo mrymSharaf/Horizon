3b1fcd27b5624db828c8316a92ef9d5923331f34
Mariam Sharaf <maryamshaider25@gmail.com>
2025-09-05 16:25:24 +0300

    add profile model to uplaod profile picture

--- main_app/models.py
+++ main_app/models.py
@@ -72,3 +72,15 @@ class VisitLike(models.Model):
     
     def __str__(self):
         return f'{self.user.username} liked {self.visit}'
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(storage=MediaCloudinaryStorage())
+
+    class Meta:
+        db_table = 'profile'
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
+    
+    
